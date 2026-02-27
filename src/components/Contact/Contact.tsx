a629da62ea5281ff2771622863f88ca3f90aedbc
coder-ali-ba <habibali8770@gmail.com>
2026-02-28 00:09:38 +0500

    rebase

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,7 +1,7 @@
 import { Container } from "./styles";
 import emailIcon from "../../assets/email-icon.svg";
 import phoneIcon from "../../assets/phone-icon.svg"
-import { Form } from "../Form/Form";
+// import { Form } from "../Form/Form";
 
 
 export function Contact(){
@@ -23,7 +23,7 @@ export function Contact(){
           <a href="tel:+923405987388">(+92) 405987388</a>
         </div>  
       </div>
-      <Form></Form>
+      {/* <Form></Form> */}
     </Container>
   )
 }
--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -19,22 +19,22 @@ export function Project() {
                 <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
               </svg>
               <div className="project-links">
-                <a href="https://github.com/CodeVinayak/MultilingualVQA" target="_blank" rel="noreferrer">
+                <a href="https://github.com/coder-ali-ba/Video-Calling-App" target="_blank" rel="noreferrer">
                   <img src={githubIcon} alt="GitHub" />
                 </a>
               </div>
             </header>
             <div className="body">
-              <h3>Multilingual VQA Vision-Language Model</h3>
+              <h3>Video-Calling App</h3>
               <p>
-                Fine-tuned Qwen2.5-VL for visual question answering across 14 languages with optimized preprocessing.
+                It uses Socket.io to handle instant, bidirectional communication between users — such as joining rooms, sending call requests, and exchanging signaling data. For video and audio streaming, technologies like WebRTC are typically used, while Socket.io manages the real-time events (connect, disconnect, mute, chat, etc.).
               </p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>Python</li>
-                <li>Qwen2.5-VL</li>
-                <li>Unsloth</li>
+                <li>React</li>
+                <li>Node.js</li>
+                <li>Socket.io</li>
               </ul>
             </footer>
           </div>
@@ -45,7 +45,7 @@ export function Project() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"> <title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
               <div className="project-links">
-                <a href="https://github.com/CodeVinayak/Serverless-Voting-Application" target="_blank" rel="noreferrer">
+                <a href="https://github.com/coder-ali-ba/Auth-BoilerPlate" target="_blank" rel="noreferrer">
                   <img src={githubIcon} alt="Visit site" /></a>
                 {/* <a href="https://vote.vinayaksingh.com" target="_blank" rel="noreferrer">
                   <img src={externalLink} alt="Visit site" />
@@ -53,10 +53,16 @@ export function Project() {
                 </div>
             </header>
             <div className="body">
-              <h3>Serverless Voting Application for Programming Languages</h3>
-              <p> Developed a serverless voting app using React, AWS Lambda, API Gateway, and DynamoDB, enabling users to view programming languages, cast votes, and access details through an interactive UI, leveraging serverless architecture for scalability and cost-efficiency. </p>
+              <h3>Authentication Boilerplate</h3>
+              <p> An authentication boilerplate using React (frontend) and Express (backend) provides a secure user login and registration system.
+
+The frontend handles user forms, protected routes, and JWT token management, while the backend manages user data, password hashing, and token generation using JWT.
+
+Multer is used to handle file uploads (such as profile images), and Cloudinary stores uploaded images securely in the cloud.
+
+This setup ensures secure authentication, protected APIs, and scalable media handling for modern web applications. </p>
             </div>
-            <footer> <ul className="tech-list"> <li>AWS Lambda</li> <li>API Gateway</li> <li>DynamoDB</li> </ul> </footer>
+            <footer> <ul className="tech-list"> <li>Express.js</li> <li>JWT</li> <li>React</li> </ul> </footer>
           </div>
         </ScrollAnimation>
 
@@ -65,23 +71,23 @@ export function Project() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"><title>Folder</title> <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path> </svg>
               <div className="project-links">
-                <a href="https://github.com/CodeVinayak/AI-Chatbot-Assistant" target="_blank" rel="noreferrer">
+                <a href="https://github.com/coder-ali-ba/AI-Chatbot-BackEnd" target="_blank" rel="noreferrer">
                   <img src={githubIcon} alt="Visit site" /> </a>
-                <a href="https://ai-chatbot-t8fn.onrender.com" target="_blank" rel="noreferrer">
+                <a href="https://ai-chatbot-front-end-three.vercel.app/" target="_blank" rel="noreferrer">
                   <img src={externalLink} alt="Visit site" /></a>
               </div>
             </header>
             <div className="body">
-              <h3>GeniusBot: AI-Powered Assistance with PDF Insight</h3>
+              <h3>GeniusBot: AI-Powered Assistance & Chatbot</h3>
               <p>
-                Developed an interactive chatbot application using Streamlit, OpenAI's GPT-3.5-turbo language model, and PyPDF2 for PDF text extraction, enabling users to ask context-based questions on uploaded PDFs and general queries.
+                It uses Google Gemini to generate human-like responses, answer questions, assist with tasks, and provide contextual conversations. The chatbot can be integrated into web or mobile applications using APIs, allowing real-time interaction.
               </p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>Streamlit</li>
-                <li>PyPDF2</li>
-                <li>LangChain</li>
+                <li>Google Gemini</li>
+                <li>Express.js</li>
+                <li>React.js</li>
               </ul>
             </footer>
           </div>
@@ -93,24 +99,24 @@ export function Project() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links">
-                <a href="https://github.com/CodeVinayak/MediBook-Medical-Appointment-Scheduler-with-Health-History" target="_blank" rel="noreferrer">
+                <a href="https://github.com/coder-ali-ba/Nexus" target="_blank" rel="noreferrer">
                   <img src={githubIcon} alt="Visit site" />
                 </a>
-                <a href="https://medibook.vinayaksingh.com" target="_blank" rel="noreferrer">
+                <a href="https://nexus-omega-rust.vercel.app/login" target="_blank" rel="noreferrer">
                   <img src={externalLink} alt="Visit site" />
                 </a>
               </div>
             </header>
             <div className="body">
-              <h3>MediBook: Medical Appointment Scheduler with Health History</h3>
-              <p>Developed a secure medical appointment management platform with CRUD functionality, utilizing role-based access control, encryption techniques, and a robust MySQL database with normalized schema design and stored procedures for efficient scheduling, data privacy, and reliable retrieval of patient records.</p>
+              <h3>Investor Entrepreneur Collab</h3>
+              <p>I implemented WebRTC-based video calling for seamless virtual meetings, built a document upload and preview system with an integrated digital signing feature, and developed a calendar module that allows users to select dates and manage meeting requests (confirm, accept, or decline).</p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>PHP</li>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>MySQL</li>
+                <li>React.js</li>
+                <li>Javascript</li>
+                <li>Tailwindcss</li>
+                <li>webRTC</li>
               </ul>
             </footer>
           </div>
@@ -124,23 +130,23 @@ export function Project() {
                 <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
               </svg>
               <div className="project-links">
-                <a href="https://github.com/CodeVinayak/Real-Time-Hand-Gesture-Driven-3D-Object-Manipulation" target="_blank" rel="noreferrer">
+                <a href="https://github.com/coder-ali-ba/ecommerce-frontend-design." target="_blank" rel="noreferrer">
                   <img src={githubIcon} alt="Visit site" />
                 </a>
-                <a href="https://youtu.be/NOm-3MynPLE" target="_blank" rel="noreferrer">
+                <a href="https://ecommerce-frontend-design-xepn.vercel.app/" target="_blank" rel="noreferrer">
                   <img src={externalLink} alt="Visit site" />
                 </a>
               </div>
             </header>
             <div className="body">
-              <h3>Real-Time Hand Gesture-Driven 3D Object Manipulation</h3>
-              <p>Developed a real-time hand gesture recognition system for intuitive 3D object manipulation. Leveraged a multimodal approach integrating OpenCV for video processing, MediaPipe for hand detection/landmark extraction, and scikit-learn for machine learning model training. Achieved 95.2% accuracy in hand gesture classification with efficient real-time performance.</p>
+              <h3>E-commerce Frontend design</h3>
+              <p>The frontend includes features such as product listings, category filters, product detail pages, shopping cart UI, responsive navigation, and a clean checkout interface — delivering a smooth and visually appealing online shopping experience.</p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>OpenCV</li>
-                <li>MediaPipe</li>
-                <li>scikit-learn</li>
+                <li>React.js</li>
+                <li>Tailwindcss</li>
+                <li>MaterialUI</li>
               </ul>
             </footer>
           </div>
@@ -154,7 +160,7 @@ export function Project() {
                 <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
               </svg>
               <div className="project-links">
-                <a href="https://github.com/CodeVinayak/Jupyter-Notebook/tree/main/Credit%20Risk%20Analysis" target="\_blank" rel="noreferrer">
+                <a href="https://github.com/coder-ali-ba/Full-Stack-QuizApp" target="\_blank" rel="noreferrer">
                   <img src={githubIcon} alt="Visit site" />
                 </a>
                 {/* <a href="https://github.com/CodeVinayak/Jupyter-Notebook/blob/main/Credit%20Risk%20Analysis/Credit_Risk_Analysis.ipynb" target="\_blank" rel="noreferrer">
@@ -163,16 +169,18 @@ export function Project() {
               </div>
             </header>
             <div className="body">
-              <h3>Credit Risk Analysis Project</h3>
+              <h3>Fullstack Quizz App</h3>
               <p>
-                Developed a highly accurate credit risk classification model using XGBoost, achieving 100% precision, recall, and F1-scores for predicting loan defaults.Implemented rigorous data preprocessing, feature engineering, and hyperparameter tuning on imbalanced credit risk data, employing ensemble methods, cross-validation, and model interpretation techniques.
+               I built a Fullstack Quiz App using HTML, CSS, and JavaScript for the frontend, with Firebase for authentication and real-time updates, and Supabase for storing quiz questions and user scores.
+
+               The app allows users to register, take quizzes, track their scores, and view a leaderboard, providing a complete interactive learning experience.
               </p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>XGBoost</li>
-                <li>Pandas</li>
-                <li>Matplotlib</li>
+                <li>Javascript</li>
+                <li>Firebase</li>
+                <li>Supabase</li>
               </ul>
             </footer>
           </div>
